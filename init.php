d36df2376c8e1a197397863ab7ad110d85bbe363
MauroSpe <mauro.spezzaferro@studenti.uniparthenope.it>
2018-02-15 16:05:46 +0100

    Create init.php

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$host 			= "localhost"; 
+	$user 			= "root"; 
+	$password_db 	= "";
+	$db_name 		= "my_yasts";
+
+	$conn = new mysqli($host,$user,$password_db,$db_name);
+
+	if ( !$conn )
+		die ( "Errore nel fase di connessione al db!".$conn->connect_error);
+
+
+
+?>
